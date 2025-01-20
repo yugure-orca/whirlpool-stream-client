686096cebfd57ed748287122ea6fbdf6caf23d75
yugure <109891005+yugure-orca@users.noreply.github.com>
2025-01-21 00:50:59 +0900

    use clap

--- examples/rust/monitor-sol-price/src/main.rs
+++ examples/rust/monitor-sol-price/src/main.rs
@@ -1,11 +1,22 @@
 use std::error::Error;
+use clap::Parser;
 use whirlpool_stream_websocket_client::{WhirlpoolStreamWebsocketClient, WhirlpoolStreamMessage, EventParam, AccountParam, event::WhirlpoolEvent, event::definition::TradeDirection};
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short = 'e', long, default_value = "wss://orcanauts-a.whirlpool-stream.pleiades.dev")]
+    endpoint: String,
+    #[arg(short = 'k', long, default_value = "demo")]
+    apikey: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
     let mut client = WhirlpoolStreamWebsocketClient::connect(
-        "wss://orcanauts-a.whirlpool-stream.pleiades.dev",
-        "demo",
+        args.endpoint.as_str(),
+        args.apikey.as_str(),
         None,
         Some(500), // This setting does not affect the stream being disconnected once a day. Client must implement reconnection logic.
         EventParam::Trade, // Traded event only
